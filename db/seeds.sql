4ac69c484db24367626a7c5af9ee80718de3ac0f
themadtaco <aatrox48@gmail.com>
2022-02-23 17:02:07 -0600

    added seeds.sql and schema.sql

---
+++ db/seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO employee (first_name, last_name)
+VALUES
+    ('Ronald', 'Donald'),
+    ('Jon', 'Snow'),
+    ('Doctor', 'Strange'),
+    ('Ricky', 'Bobby'),
+    ('Biggie', 'Smalls'),
+    ('Snoop', 'Dogg'),
+    ('The', 'Hound'),
+    ('Amy', 'Winehouse'),
+    ('General', 'Grevious'),
+    ('Chimera', 'Grey');
+INSERT INTO role (title, salary)
+VALUES
+    ('Manager', 100000.00),
+    ('Janitor', 35000.00),
+    ('Customer Service', 40000.00),
+    ('Jr Developer', 50000.00),
+    ('Sr Developer', 75000.00);
+INSERT INTO department (name)
+VALUES
+    ('Developers'),
+    ('Management'),
+    ('Service');
